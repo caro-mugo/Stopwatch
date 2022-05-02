ad645aff601ba624ca386a0fb061019cf56521a0
caro-mugo <mugonycaroline@gmail.com>
2022-05-02 12:08:08 +0300

    stopwatch

--- stopwatch.py
+++ stopwatch.py
@@ -1,14 +1,11 @@
 import time
-
-print("This is printed immediately.")
-time.sleep(2.4)
-print("This is printed after 2.4 seconds.")
-import time
-seconds = time.time()
-print("Seconds since epoch =", seconds)	
-import time
-
-result = time.localtime(1545925769)
-print("result:", result)
-print("\nyear:", result.tm_year)
-print("tm_hour:", result.tm_hour)
+while True:
+    
+        input("Press Enter to continue and ctrl+C to exit the stopwatch")
+        start_time=time.time()
+        print("Stopwatch has started")
+        while True:
+            print("Time elapsed:",round(time.time()-start_time,0),'secs',end='\n')
+            time.sleep(1)
+            
+            
